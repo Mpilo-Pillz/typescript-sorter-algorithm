fd60bbbeafaa5860e0d75b2e7e5218e141379a48
Fikasentani <mpilo.dlamini@yahoo.com>
2021-01-06 08:52:09 +0200

    bad implementation using insanceof and typeof

--- build/index.js
+++ build/index.js
@@ -4,13 +4,20 @@ var Sorter = /** @class */ (function () {
         this.collection = collection;
     }
     Sorter.prototype.sort = function () {
+        //    const length = this.collection.length
         var length = this.collection.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+                // only runs if collection is array of numbers
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                // if collection is a string
+                if (typeof this.collection === 'string') {
                 }
             }
         }
