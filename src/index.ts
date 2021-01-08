d77740feee4d795c248f4a8479f5c7bc3eadff6a
Fikasentani <mpilo.dlamini@yahoo.com>
2021-01-08 20:00:54 +0200

    adding linked list

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0])
 console.log(numbersCollection);
@@ -10,7 +11,19 @@ numbersSorter.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('Xaayb');
+console.log(charactersCollection);
+
 const charactersSorter = new Sorter(charactersCollection);
 charactersSorter.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+linkedList.print();
 
